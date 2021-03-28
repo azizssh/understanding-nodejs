2a6ade3bdc186324bae95960581e10a9d652fc26
Aziz A <aliqsson@gmail.com>
2021-03-28 18:12:33 -0500

    update files

--- 6-function-constructors.js
+++ 6-function-constructors.js
@@ -3,5 +3,11 @@ function Person(firstName, lastName) {
     this.lastName = lastName;
 }
 
+Person.prototype.greet = function() {
+    console.log('Hello', this.firstName, this.lastName);
+}
+
 const john = new Person('John', 'Doe');
 console.log(john.firstName)
+
+john.greet();
